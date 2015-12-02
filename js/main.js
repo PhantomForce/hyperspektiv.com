31035448d3da4f623c7f987a063ef3b72807273b
Allan Lavell <alavell@gmail.com>
2015-12-02 16:13:08 -0400

    Put in anchor links with smooth scrolling.

--- js/main.js
+++ js/main.js
@@ -15,3 +15,18 @@ $('.scroll').on('click', function(e){
         scrollTop : $(this.hash).offset().top
     }, 1500);
 });
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
